840b76ed89019605af7cfe9c42bd42e131b1dd41
Salsawork <salsabilawork@gmail.com>
2025-09-29 16:56:49 +0700

    update form register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -50,9 +50,9 @@
                     @error('name')
                         <p class="text-red-500 text-sm mb-4">{{ $message }}</p>
                     @enderror
-                    <input type="text" placeholder="Phone number or Email " name="email"
+                    <input type="text" placeholder="Phone number or Email " name="username"
                         class="mb-4 w-full rounded-md border border-gray-500 bg-transparent px-3 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                    @error('email')
+                    @error('username')
                         <p class="text-red-500 text-sm mb-4">{{ $message }}</p>
                     @enderror
                     <input type="password" placeholder="Password" name="password"
